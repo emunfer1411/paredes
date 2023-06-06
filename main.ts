5541477c3e89d1c7ce45ebd99cb68442fe31e83e
emunfer1411 <130557119+emunfer1411@users.noreply.github.com>
2023-06-06 13:42:00 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,45 @@
+let Velocidad = 40
+let Velocidad_de_Giro = 40
+let obstaculo = 0
+basic.forever(function () {
+    if (maqueen.Ultrasonic(PingUnit.Centimeters) < 5) {
+        obstaculo = obstaculo + 1
+        if (obstaculo == 1) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(1550)
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+        if (obstaculo == 2) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(2000)
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+        if (obstaculo == 3) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(2100)
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+        if (obstaculo == 4) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(1900)
+            maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 40)
+            basic.pause(1000)
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+        if (obstaculo == 5) {
+            maqueen.motorStop(maqueen.Motors.All)
+            maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, Velocidad_de_Giro)
+            basic.pause(1900)
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+        if (obstaculo == 6) {
+            maqueen.motorStop(maqueen.Motors.All)
+        }
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, Velocidad)
+    }
+})
